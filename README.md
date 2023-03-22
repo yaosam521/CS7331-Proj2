5873a925c97dcebe43cde819a0f928a08e4be2ce
Zinoope <brahmi.medzineeddine@gmail.com>
2023-03-22 15:46:44 -0500

    updated dataset added, new description, and population desity aggregation

--- README.md
+++ README.md
@@ -2,18 +2,35 @@
 Project 02 for Data Mining calss to apply Covid plus Census dataset analysis in Ohio State
 
 # Retreived Data
-The data was retreived on 3/22/2023 using Google cloud services
+The data was retreived on 3/22/2023 from Google cloud website, using the following 3 diffrent datasets:
 
-you can find the used USAFacts US Coronavirus Database in the following link:
-https://console.cloud.google.com/marketplace/details/usafacts-public-data/covid19-us-cases?filter=solution-type:dataset&filter=category:covid19&id=3eaff9c5-fbaf-47bb-a441-89db1e1395ab&project=cs7331-prij01
+1. USAFacts US Coronavirus Database:
+   Publisher: USAFacts
+You can review it using the following link:
+https://console.cloud.google.com/marketplace/details/usafacts-public-data/covid19-us-cases?filter=solution-type:dataset&filter=category:covid19&id=3eaff9c5-fbaf-47bb-a441-89db1e1395ab
 
-This dataset was pulled with 3/5/2023 updates to be combined with USA Census dataset of 2020 (with 5 years average) in order to 
+2. US Census Data
+   Publisher: United States Census Bureau
+You can review it using the following link:
+https://console.cloud.google.com/marketplace/details/united-states-census-bureau/us-census-data
 
-The resulting query was the following:
+3. Census Bureau US Boundaries:
+   Publisher: United States Census Bureau
+You can review it using the following link:
+https://console.cloud.google.com/marketplace/details/united-states-census-bureau/us-geographic-boundaries
+
+The dataset was pulled dataset (located in the data folder) correspands to 3/5/2023 updates of covid-19 data combined with USA Census dataset (2020 [5 years average]) and US boundries attributes.
+
+To make sure that the pulled results corresponds only to Ohio counties, a state condition was added to the query.
+
+The final query:
+
 SELECT *
 FROM `bigquery-public-data.covid19_usafacts.summary` covid19
 JOIN `bigquery-public-data.census_bureau_acs.county_2020_5yr` acs
 ON covid19.county_fips_code = acs.geo_id
+JOIN bigquery-public-data.geo_us_boundaries.counties geo
+ON geo.county_fips_code=acs.geo_id
 WHERE date = DATE_SUB(CURRENT_DATE(), INTERVAL 17 day) and state="OH"
 
-the interval of days can be increased to find previous dates records if desired
+Note: The interval of days can be increased/decreased to find previous/newer dates records if desired.
--- src/Covid_Census.R
+++ src/Covid_Census.R
@@ -1,11 +1,11 @@
 ### PART I: cleaning and Visualization ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-## Step I-1: Loading the Dataset ----------------------------------------------------
+## Step I-01: Loading the Dataset ----------------------------------------------------
 #load the csv file/ check how many missing values do we have
 library("tidyverse")
 library("DT")
 library("cowplot")
-cases <- read_csv("../data/Ohio Covid 03-05 + Census 2020-5yrs.csv")
+cases <- read_csv("../data/Ohio Covid 03-05 + Census 2020-5yrs + Geo Boundaries.csv")
 cases <- cases %>% mutate_if(is.character, factor)
 cases
 # check for NA values
@@ -13,71 +13,32 @@ is.na(cases) %>% sum()
 
 
 
-## Step I-02: Aggregation and Normalization ----------------------------------------
+## Step I-02: Aggregation, Normalization and Selection -----------------------------
 cases_filtered <- cases %>% mutate(
-  female_under_40_ratio= (female_under_5 +
-    female_5_to_9 +
-    female_10_to_14 +
-    female_15_to_17 +
-    female_18_to_19 +
-    female_20 +
-    female_21 +
-    female_22_to_24 +
-    female_25_to_29 +
-    female_30_to_34 +
-    female_35_to_39) / total_pop,
   
-  female_pop_P100=female_pop/total_pop*100,
-  
-  male_under_40_ratio= (male_under_5 +
-    male_5_to_9 +
-    male_10_to_14 +
-    male_15_to_17 +
-    male_18_to_19 +
-    male_20 +
-    male_21 +
-    male_22_to_24 +
-    male_25_to_29 +
-    male_30_to_34 +
-    male_35_to_39) / total_pop,
-  
-  male_pop_P100=male_pop/total_pop*100,
-  
-  asian_pop_P1000=asian_pop/total_pop*1000,
   black_pop_P1000=black_pop/total_pop*1000,
-  hispanic_pop_P1000=hispanic_pop/total_pop*1000,
-  white_pop_P1000=white_pop/total_pop*1000,
   deaths_P1000 = deaths/total_pop*1000,
   confirmed_cases_P1000= confirmed_cases/total_pop*1000,
-  walked_to_work_P1000 = walked_to_work/total_pop*1000,
   commuters_by_public_transportation_P1000 = commuters_by_public_transportation/total_pop*1000,
-  commuters_by_carpool_P1000 = commuters_by_carpool/total_pop*1000,
-  commuters_drove_alone_P1000 = commuters_drove_alone/total_pop*1000,
+  pop_density_Pkm= total_pop * 10^6 /area_land_meters
 )
 
 cases_cleaned <- cases_filtered %>% select(county_name,
                                            confirmed_cases_P1000,
                                            deaths_P1000,
                                            total_pop,
-                                           asian_pop_P1000,
                                            black_pop_P1000,
-                                           hispanic_pop_P1000,
-                                           white_pop_P1000,
-                                           walked_to_work_P1000,
                                            commuters_by_public_transportation_P1000,
-                                           commuters_by_carpool_P1000,
-                                           commuters_drove_alone_P1000,
-                                           female_pop_P100,
-                                           female_under_40_ratio,
-                                           male_pop_P100,
-                                           male_under_40_ratio,
                                            median_age,
                                            median_income,
-                                           income_per_capita
+                                           income_per_capita,
+                                           pop_density_Pkm
 ) 
 
-rm(cases, cases_filtered)
+# check for NA values
+is.na(cases_cleaned) %>% sum()
 cases_cleaned
+rm(cases, cases_filtered)
 
 ## Step I-03:  Data visualization [deaths and detected maps] -----------------------
 counties <- as_tibble(map_data("county"))
@@ -88,7 +49,7 @@ cases_OH <- cases_cleaned %>% mutate(county = county_name %>%
 
 counties_OH_clust <- counties_OH %>% left_join(cases_OH)
 rm(counties,counties_OH,cases_OH)
-par(mfrow=c(1,2))
+
 
 deaths_map <- ggplot(counties_OH_clust, aes(long, lat)) + 
   geom_polygon(aes(group = group, fill = deaths_P1000)) +
@@ -102,9 +63,16 @@ cases_map <- ggplot(counties_OH_clust, aes(long, lat)) +
   labs(title = "Cases in Ohio State", fill = "Cases per 1000")
 
 plot_grid(deaths_map, cases_map, nrow = 1, ncol = 2)
+rm(counties_OH_clust,cases_map, deaths_map)
 
 ### PART II: clustering ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ## Step II-01:  K-Means -------------------------------------------------------------
+cases_to_cluster <- subset(cases_cleaned, select = (c(median_income,
+                                                      income_per_capita,
+                                                      median_age,
+                                                      black_pop_P1000,
+                                                      pop_density_Pkm
+                                                      )))
 
 
